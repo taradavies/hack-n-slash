fa8872166de8ceb3cb9c8432772d21f761f3640e
Tara Davies <tarawafflebear@gmail.com>
2022-08-15 17:10:42 +0800

    imported particle fx package and made enemy play one when hit

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,12 +4,34 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour, ITakeHit
 {
+    [SerializeField] GameObject _hitParticles;
+    [SerializeField] int _maxHealth = 3;
+
+    int _currentHealth;
     Animator _animator;
     void Awake()
     {
         _animator = GetComponentInChildren<Animator>();
     }
+    // performance benefit because we don't want to constantly pool them
+    void OnEnable()
+    {
+        _currentHealth = _maxHealth;
+    }
     public void TakeHit(Character hitBy)
+    {
+        _currentHealth --;
+
+        _animator.SetTrigger("Hit");
+        Instantiate(_hitParticles, transform.position + new Vector3(2, 2, -0.5f), Quaternion.identity);    
+
+        if (_currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
     {
         _animator.SetTrigger("Die");
         Destroy(gameObject, 1.2f);
