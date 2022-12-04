1dcde4f163ec99a8d0bef148dcd9dc47940ad0b7
Tara Davies <tarawafflebear@gmail.com>
2022-12-04 15:09:35 +0800

    created pooling for hit particles

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -9,7 +9,7 @@ using UnityEngine.AI;
 [RequireComponent(typeof(Attacker))]
 public class Enemy : PooledMonoBehaviour, ITakeHit
 {
-    [SerializeField] GameObject _hitParticles;
+    [SerializeField] PooledMonoBehaviour _hitParticles;
     [SerializeField] int _maxHealth = 3;
     bool IsDead => _currentHealth <= 0;
 
@@ -91,7 +91,8 @@ public class Enemy : PooledMonoBehaviour, ITakeHit
         _currentHealth --;
 
         _animator.SetTrigger("Hit");
-        Instantiate(_hitParticles, transform.position + new Vector3(2, 2, -0.5f), Quaternion.identity);    
+
+        _hitParticles.Get<PooledMonoBehaviour>(transform.position + new Vector3(0, 2, 0), Quaternion.identity);   
 
         if (_currentHealth <= 0)
         {
